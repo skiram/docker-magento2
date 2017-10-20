d4de8ca41fa38218c518911857bf8127cf55b71b
Nicolas BATTY <nicolas.batty@dragonbleu.fr>
2017-10-20 16:28:19 +0200

    Add CLI and cron support

---
+++ cron/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+{ \
+    echo "* * * * * docker /usr/local/bin/php ${MAGE_ROOT}/bin/magento cron:run 2>&1 | grep -v Ran jobs by schedule >> ${MAGE_ROOT}/var/log/cron.log"; \
+    echo "* * * * * docker /usr/local/bin/php ${MAGE_ROOT}/update/cron.php >> ${MAGE_ROOT}/var/log/cron.log"; \
+    echo "* * * * * docker /usr/local/bin/php ${MAGE_ROOT}/bin/magento setup:cron:run >> ${MAGE_ROOT}/var/log/cron.log"; \
+} > /etc/cron.d/magento-cron
+
+echo "cron.* /var/log/cron.log" > /etc/rsyslog.d/cron.conf
+service rsyslog start
+
+exec "$@"
