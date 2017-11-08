727c78ff20d6bd4907f7a597b95c2e8687d6b3c0
Nicolas BATTY <nicolas.batty@dragonbleu.fr>
2017-11-08 17:05:39 +0100

    Add dynamic port support for Varnish

--- varnish/start.sh
+++ varnish/start.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-exec bash -c "varnishd -F -u varnish -f /etc/varnish/default.vcl -p default_ttl=3600 -p default_grace=3600"
+exec bash -c "varnishd -a :$LISTEN_PORT -F -u varnish -f /etc/varnish/default.vcl -p default_ttl=3600 -p default_grace=3600"
