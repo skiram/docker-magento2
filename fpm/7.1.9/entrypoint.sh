7d4e45baa8444f4b4c7be6630f9af047dd8e8217
skiram <saber.kiram@gmail.com>
2019-12-12 14:58:44 +0100

    Update docker php 7.1.9

---
+++ fpm/7.1.9/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+XDEBUG_INI_FILE="/usr/local/etc/php/conf.d/ext-xdebug.ini"
+XDEBUG_INI_FILE_DIS="${XDEBUG_INI_FILE}.disabled"
+
+if [ "$XDEBUG_ENABLED" = "true" ]; then
+    if [ -f "$XDEBUG_INI_FILE_DIS" ]; then
+        sudo mv $XDEBUG_INI_FILE_DIS $XDEBUG_INI_FILE
+    fi
+else
+    if [ -f "$XDEBUG_INI_FILE" ]; then
+        sudo mv $XDEBUG_INI_FILE $XDEBUG_INI_FILE_DIS
+    fi
+fi
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+    set -- php-fpm "$@"
+fi
+
+exec "$@"
